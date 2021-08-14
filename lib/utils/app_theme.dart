ebc185bbdc9a69016ce8b0211abaea4303d070ac
HosseinAsadi <HosseinA.9876@gmail.com>
2021-08-14 12:54:50 +0430

    added fonts

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -16,7 +16,7 @@ class AppTheme {
     primaryColor: _green,
     scaffoldBackgroundColor: Colors.white,
     checkboxTheme: _checkBoxTh,
-    fontFamily: 'IBM_Plex',
+    fontFamily: 'Inter',
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       backgroundColor: Colors.white,
       elevation: 3.0,
@@ -35,7 +35,7 @@ class AppTheme {
     primaryColor: _green,
     scaffoldBackgroundColor: Colors.black,
     checkboxTheme: _checkBoxTh,
-    fontFamily: 'IBM_Plex',
+    fontFamily: 'Inter',
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       backgroundColor: Color(0xff151515),
       elevation: 3.0,
